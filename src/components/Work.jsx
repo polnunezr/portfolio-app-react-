789759afd629be5900ac781f2d29064591fee7c4
Pol Núñez <polnunez@hotmail.es>
2024-05-23 17:22:08 +0200

    Change vue to wordpress
    Change vue to wordpress

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -95,7 +95,7 @@ const Work = () => {
             {/* Hover Effects */}
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                Vue Application
+                Wordpress Application
               </span>
               <div className="pt-8 text-center">
                 <a href="https://reclube.es/">
